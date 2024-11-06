6578f5e86dfa8ee365fa98127ede1f21732b41d6
Kritika Singh <kritikasingh6881@gmail.com>
2024-11-06 14:40:32 +0530

    Update views.py

--- stories/views.py
+++ stories/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Story
 from .serializers import StorySerializer
+from rest_framework.permissions import AllowAny
 
 # Create your views here.
 class StoryViewSet(viewsets.ModelViewSet):
   queryset = Story.objects.all() #This defines the data that the view will work with. Here, it retrieves all records from the Story model. When someone makes a request to this API, this queryset determines which stories are accessible.
   serializer_class = StorySerializer #This tells the viewset to use StorySerializer to handle data conversion. The serializer will convert Story instances into JSON format for API responses and handle incoming JSON data to create or update Story instances in the database.
+  permission_classes = [AllowAny] #Allow any user to access the API
